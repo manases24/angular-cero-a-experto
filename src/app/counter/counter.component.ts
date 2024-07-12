f1f74c74dea479834631413467c48a81f04fb935
mnsh5 <hugo.orlando2050@gmail.com>
2024-07-12 21:45:09 +0000

    se agrega counter component

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+})
+export class CounterComponent {
+  public counter: number = 0;
+
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
+
+  resetBy(value: number): void {
+    this.counter = value;
+  }
+}
