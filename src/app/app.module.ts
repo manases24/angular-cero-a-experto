5dab46d54b405d352f1e062d1ea5a197428038a9
mnsh5 <hugo.orlando2050@gmail.com>
2024-07-13 20:22:36 +0000

    se agrega Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from '../app/heroes/heroes.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
 
 @NgModule({
   declarations: [AppComponent],
