348f7698bffa0a0aea6be6f206c929f861dd7b04
mnsh5 <hugo.orlando2050@gmail.com>
2024-07-15 20:05:07 +0000

    se agrega nuevas actualizaciones (components y services)

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GifsService {
+  private _tagHistory: string[] = [];
+
+  constructor() {}
+
+  get tagsHistory() {
+    return this._tagHistory;
+  }
+}
