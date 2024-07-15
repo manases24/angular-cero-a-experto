89a6a46c5ef15be531c2a976b40b9985530ca6dd
mnsh5 <hugo.orlando2050@gmail.com>
2024-07-15 19:32:33 +0000

    se agrega nuevas actualizaciones

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    HomePageComponent
+  ],
+  exports: [],
+  imports: [CommonModule],
 })
-export class GifsModule { }
+export class GifsModule {}
