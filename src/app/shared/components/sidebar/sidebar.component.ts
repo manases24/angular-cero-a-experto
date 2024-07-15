4106d7b1a1c2053d5429e50f3c15626b59397bfa
mnsh5 <hugo.orlando2050@gmail.com>
2024-07-15 20:30:44 +0000

    se agrega nuevas actualizaciones (components y services)

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -8,4 +8,8 @@ import { GifsService } from '../../../gifs/services/gifs.service';
 })
 export class SidebarComponent {
   constructor(private gifsService: GifsService) {}
+
+  get tags() {
+    return this.gifsService.tagsHistory;
+  }
 }
