c8b49fee86efa1f6e5013c5f1e3c7ce51e8bd47f
mnsh5 <hugo.orlando2050@gmail.com>
2024-07-15 19:12:07 +0000

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CounterModule } from './counter/counter.module';
-import { HeroesModule } from '../app/heroes/heroes.module';
-import { AfaModule } from '../app/afa/afa.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent
+  ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    CounterModule,
-    HeroesModule,
-    AfaModule,
+    AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
