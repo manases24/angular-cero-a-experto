4d937641e32614a2bf59c3c09be2a5ff595cab5c
mnsh5 <hugo.orlando2050@gmail.com>
2024-07-17 15:30:54 +0000

    se agregan nuevos cambios

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -11,7 +11,9 @@ export class GifsService {
   private giphyApiKey: string = 'HX4npOV3yntlrXLaChcORpp6ZJO12xe2';
   private giphyUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+  }
 
   get tagsHistory() {
     return [...this._tagHistory];
@@ -24,6 +26,20 @@ export class GifsService {
     }
     this._tagHistory.unshift(tag);
     this._tagHistory = this.tagsHistory.splice(0, 10);
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify(this._tagHistory));
+  }
+
+  private loadLocalStorage(): void {
+    if (!localStorage.getItem('history')) {
+      return;
+    }
+    this._tagHistory = JSON.parse(localStorage.getItem('history')!);
+    if (this._tagHistory.length === 0) return;
+    this.searchTag(this._tagHistory[0]);
   }
 
   searchTag(tag: string): void {
