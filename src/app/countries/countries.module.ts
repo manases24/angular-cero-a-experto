cb43c70096ed4c027e0e7db4184147b34c023c6a
mnsh5 <hugo.orlando2050@gmail.com>
2024-07-17 19:54:38 +0000

    se agrega nuevos componentes y rutas

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -4,18 +4,15 @@ import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.
 import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
 import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
 import { CountryPageComponent } from './pages/country-page/country-page.component';
-
-
+import { CountriesRoutingModule } from './countries-routing.module';
 
 @NgModule({
   declarations: [
     ByCapitalPageComponent,
     ByCountryPageComponent,
     ByRegionPageComponent,
-    CountryPageComponent
+    CountryPageComponent,
   ],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, CountriesRoutingModule],
 })
-export class CountriesModule { }
+export class CountriesModule {}
