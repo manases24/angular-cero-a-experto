690c4fe525a99c03420d137ae189826138151ea9
mnsh5 <hugo.orlando2050@gmail.com>
2024-07-17 19:26:37 +0000

    se agrega nuevos componentes y rutas

--- src/app/shared/pages/about-page/about-page.component.ts
+++ src/app/shared/pages/about-page/about-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-about-page',
+  selector: 'shared-about-page',
   templateUrl: './about-page.component.html',
 })
 export class AboutPageComponent {}
