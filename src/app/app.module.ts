870ec9cb936b7bbb245536cd0148aa9fd3df3ada
mnsh5 <hugo.orlando2050@gmail.com>
2024-07-17 17:18:45 +0000

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
-import { GifsModule } from '../app/gifs/gifs.module';
-import { SharedModule } from '../app/shared/shared.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, GifsModule, SharedModule],
-  providers: [provideHttpClient(withFetch())],
-  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
