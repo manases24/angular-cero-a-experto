ee4fb8f12aaabca99796d487f20551245aa55b5f
mnsh5 <hugo.orlando2050@gmail.com>
2024-07-17 12:30:24 +0000

    se agrega validacioes al servicio

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -12,7 +12,17 @@ export class GifsService {
     return [...this._tagHistory];
   }
 
-  searchTag(tag: string): void {
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase();
+    if (this._tagHistory.includes(tag)) {
+      this._tagHistory = this._tagHistory.filter((oldTag) => oldTag !== tag);
+    }
     this._tagHistory.unshift(tag);
+    this._tagHistory = this.tagsHistory.splice(0, 10);
+  }
+
+  searchTag(tag: string): void {
+    if (tag.length === 0) return;
+    this.organizeHistory(tag);
   }
 }
